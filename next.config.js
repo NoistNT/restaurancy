920fbc150943bb8b068da44245f085aee9a4101a
NoistNT <arielgnr23@gmail.com>
2023-11-16 15:59:07 -0300

    update eslint

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ const nextConfig = {
   experimental: {
     ppr: false
   }
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
-import type {Metadata} from "next";
+import type {Metadata} from 'next'
 
-import "./globals.css";
+import Footer from './components/Footer'
+import './globals.css'
 
 export const metadata: Metadata = {
-  title: "Restaurancy",
-};
+  title: 'Restaurancy'
+}
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
@@ -12,10 +13,8 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
       <body className="container m-auto grid min-h-screen grid-rows-[auto,1fr,auto] px-4">
         <header className="text-xl font-bold leading-[3rem]">Restaurancy</header>
         <main className="py-8">{children}</main>
-        <footer className="text-center leading-[3rem] opacity-70">
-          © {new Date().getFullYear()} Restaurancy
-        </footer>
+        <Footer />
       </body>
     </html>
-  );
+  )
 }
