b5813fab7e1aafdef52a7b13f1a5debcafb8a0fd
NoistNT <arielgnr23@gmail.com>
2023-11-17 22:36:15 -0300

    fix lint in root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,15 @@
-import type {Metadata} from 'next'
+import type { Metadata } from 'next'
 
-import Footer from './components/Footer'
 import './globals.css'
+import Footer from './components/footer'
 
 export const metadata: Metadata = {
-  title: 'Restaurancy'
+  title: 'Restaurancy',
+  description: 'The best restaurants in the world',
+  keywords: ['restaurant', 'food', 'eat', 'dinner', 'lunch']
 }
 
-export default function RootLayout({children}: {children: React.ReactNode}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className="container m-auto grid min-h-screen grid-rows-[auto,1fr,auto] px-4">
