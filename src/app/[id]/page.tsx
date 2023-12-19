11c19b5cdaf67cb030d11f1c66671c35e79f5f1a
NoistNT <arielgnr23@gmail.com>
2023-12-19 20:15:36 -0300

    Update characters page component

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -2,6 +2,8 @@ import Link from 'next/link'
 
 import api from '@/api'
 
+import Card from '../components/card'
+
 export async function generateMetadata({
   params: { id }
 }: {
@@ -11,7 +13,8 @@ export async function generateMetadata({
 
   return {
     title: `${restaurant.name} - Restaurancy`,
-    description: restaurant.description
+    description: restaurant.description,
+    metadataBase: new URL('https://localhost:3000')
   }
 }
 
@@ -23,8 +26,9 @@ export async function generateStaticParams() {
   }))
 }
 
-// Mostramos un 404 si el restaurante no existe
-// Por defecto se muestra un error de server 500
+// Por defecto se muestra un error de server 500 -> true
+// Si queremos mostrar un 404 si el restaurante no existe
+// Para cualquier ruta no definida en generateStaticParams -> false
 // true -> 500
 // false -> 404
 // export const dynamicParams = false
@@ -41,24 +45,7 @@ export default async function RestaurantPage({
       <Link href="/" prefetch={false}>
         Go back
       </Link>
-      <article>
-        <img
-          alt={restaurant.name}
-          className="mb-3 h-[300px] w-full object-cover"
-          src={restaurant.image}
-        />
-        <h2 className="inline-flex gap-2 text-lg font-bold">
-          <span>{restaurant.name}</span>
-          <small className="inline-flex gap-1">
-            <span>⭐</span>
-            <span>{restaurant.score}</span>
-            <span className="font-normal opacity-75">
-              ({restaurant.ratings})
-            </span>
-          </small>
-        </h2>
-        <p className="opacity-90">{restaurant.description}</p>
-      </article>
+      <Card restaurant={restaurant} />
     </section>
   )
 }
