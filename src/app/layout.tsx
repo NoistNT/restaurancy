8d95cdffefbd595a4b56761403ebae9f1e3083a8
NoistNT <arielgnr23@gmail.com>
2023-12-19 20:16:25 -0300

    Add link to home in main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 
 import './globals.css'
+import Link from 'next/link'
+
 import Footer from './components/footer'
 
 export const metadata: Metadata = {
@@ -18,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className="container m-auto grid min-h-screen grid-rows-[auto,1fr,auto] px-4">
         <header className="text-xl font-bold leading-[3rem]">
-          Restaurancy
+          <Link href="/">Restaurancy</Link>
         </header>
         <main className="py-8">{children}</main>
         <Footer />
