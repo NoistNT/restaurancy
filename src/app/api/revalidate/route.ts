9a46b0507cc55630776b7e9cc6a8385146f32a96
NoistNT <arielgnr23@gmail.com>
2023-12-20 17:25:06 -0300

    Add manual revalidation

---
+++ src/app/api/revalidate/route.ts
@@ -0,0 +1,11 @@
+import type { NextRequest } from 'next/server'
+
+import { revalidatePath } from 'next/cache'
+
+export async function GET(req: NextRequest) {
+  const path = req.nextUrl.searchParams.get('path') || '/'
+
+  revalidatePath(path)
+
+  return Response.json({ success: true })
+}
