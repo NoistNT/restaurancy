326f654d5d6eccf75c4cad2f9004243077b6d4c3
NoistNT <arielgnr23@gmail.com>
2023-12-21 19:11:53 -0300

    Add searchbox to search restaurants through supabase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 import api from '@/api'
 import Card from '@/components/card'
 
-export default async function Home() {
-  const restaurants = await api.list()
+export default async function Home({
+  searchParams
+}: {
+  searchParams: { name: string }
+}) {
+  const { name } = searchParams
+  const restaurants = await api.list(name)
 
   return (
     <section className="grid grid-cols-1 gap-12 md:grid-cols-2 lg:grid-cols-3">
