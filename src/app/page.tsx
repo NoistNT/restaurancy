8edf94d7a56a2bf1743bf11929e4137edd11651b
NoistNT <arielgnr23@gmail.com>
2023-12-22 12:54:23 -0300

    Render message when a restaurant is not found

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import api from '@/api'
-import Card from '@/components/card'
+import Card from '@/components/ui/RestaurantCard/card'
 
 export default async function Home({
   searchParams
@@ -8,6 +8,16 @@ export default async function Home({
 }) {
   const { name } = searchParams
   const restaurants = await api.list(name)
+
+  if (!restaurants.length) {
+    return (
+      <div className="text-md flex h-[80vh] items-center justify-center text-center opacity-70">
+        {`${name[0].toUpperCase()}${name.slice(
+          1
+        )} is not an affiliated restaurant yet.`}
+      </div>
+    )
+  }
 
   return (
     <section className="grid grid-cols-1 gap-12 md:grid-cols-2 lg:grid-cols-3">
